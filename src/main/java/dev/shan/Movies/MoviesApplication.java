cb3826dbeeda72f6be4ce8210ed4f56544f0bfe2
Shangeevan_Mukunthan <shan8rc@gmail.com>
2023-07-09 04:13:12 +0530

    Database cluster connected in secure way

--- src/main/java/dev/shan/Movies/MoviesApplication.java
+++ src/main/java/dev/shan/Movies/MoviesApplication.java
@@ -6,16 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
-
-	@GetMapping("/")
-	public String root(){
-		return "Hello world";
-	}
-
 }
