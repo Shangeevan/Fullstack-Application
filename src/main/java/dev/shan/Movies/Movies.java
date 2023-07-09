cf464af7f473fa1ba692ac6933db30e102f38913
Shangeevan_Mukunthan <shan8rc@gmail.com>
2023-07-09 11:21:20 +0530

    Movies Data created

---
+++ src/main/java/dev/shan/Movies/Movies.java
@@ -0,0 +1,33 @@
+package dev.shan.Movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "movies")
+public class Movies {
+    @Id
+    private ObjectId id;
+
+    private String imdbId;
+
+    private String title;
+
+    private String releaseDate;
+
+    private String trailerLink;
+
+    private String poster;
+
+    private List<List> genres;
+
+    private List<List> backdrops;
+}
