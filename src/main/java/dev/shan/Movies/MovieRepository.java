e9a5302bb3a6000800de1bf95b028d8ce7618542
Shangeevan_Mukunthan <shan8rc@gmail.com>
2023-07-09 17:53:43 +0530

    basic controller and repository created

---
+++ src/main/java/dev/shan/Movies/MovieRepository.java
@@ -0,0 +1,9 @@
+package dev.shan.Movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movies, ObjectId> {
+}
